19e09c33b2716a52934620b34461d46eb4460a9b
Venik2405 <venik2405@yandex.ru>
2023-03-05 15:43:08 +0300

    Class Main added

---
+++ src/main/java/com/innowise/ballsorter/runner/Main.java
@@ -0,0 +1,33 @@
+package com.innowise.ballsorter.runner;
+
+import com.innowise.ballsorter.entity.comparator.BallTypeComparator;
+import com.innowise.ballsorter.utils.SurfaceType;
+import com.innowise.ballsorter.entity.Ball;
+import com.innowise.ballsorter.entity.FootballBall;
+import com.innowise.ballsorter.entity.VolleyballBall;
+import com.innowise.ballsorter.utils.BallType;
+import com.innowise.ballsorter.utils.Color;
+import com.innowise.ballsorter.utils.Season;
+import com.innowise.ballsorter.entity.comparator.WeightComparator;
+import com.innowise.ballsorter.sorter.impl.QuickSorter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Ball> list = new ArrayList<>();
+        list.add(new VolleyballBall(Color.BLACK, 12, 200, BallType.FOOTBALL, SurfaceType.ASPHALT));
+        list.add(new FootballBall(Color.BLACK, 14, 200, BallType.FOOTBALL, Season.SUMMER));
+        list.add(new VolleyballBall(Color.WHITE, 10, 200, BallType.VOLLEYBALL, SurfaceType.SAND));
+        list.add(new FootballBall(Color.GREEN, 10, 200, BallType.RUGBY, Season.WINTER));
+
+//        list.sort(new BallTypeComparator());
+//        MergeSorter.mergeSort(list, new WeightComparator().thenComparing(new SizeComparator()));////////////////////
+//        QuickSorter.quickSort(list, new WeightComparator().thenComparing(new SizeComparator()));
+//        HeapSorter.heapSort(list, new WeightComparator().thenComparing(new BallTypeComparator()));
+        var sorter = new QuickSorter();
+        sorter.sort(list, new WeightComparator().thenComparing(new BallTypeComparator()));
+        System.out.println(list);
+    }
+}
