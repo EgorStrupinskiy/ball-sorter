af1d5907e23399a903cab606712450fabadef312
Venik2405 <venik2405@yandex.ru>
2023-03-05 17:50:54 +0300

    All sorters covered by tests

--- src/main/java/com/innowise/ballsorter/runner/Main.java
+++ src/main/java/com/innowise/ballsorter/runner/Main.java
@@ -1,14 +1,13 @@
 package com.innowise.ballsorter.runner;
 
-import com.innowise.ballsorter.entity.comparator.BallTypeComparator;
-import com.innowise.ballsorter.utils.SurfaceType;
 import com.innowise.ballsorter.entity.Ball;
 import com.innowise.ballsorter.entity.FootballBall;
 import com.innowise.ballsorter.entity.VolleyballBall;
+import com.innowise.ballsorter.entity.comparator.ColorComparator;
+import com.innowise.ballsorter.sorter.impl.HeapSorter;
 import com.innowise.ballsorter.utils.Color;
 import com.innowise.ballsorter.utils.Season;
-import com.innowise.ballsorter.entity.comparator.WeightComparator;
-import com.innowise.ballsorter.sorter.impl.QuickSorter;
+import com.innowise.ballsorter.utils.SurfaceType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,17 +15,14 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Ball> list = new ArrayList<>();
-        list.add(new VolleyballBall(Color.BLACK, 12, 200,  SurfaceType.ASPHALT));
-        list.add(new FootballBall(Color.BLACK, 14, 200,  Season.SUMMER));
-        list.add(new VolleyballBall(Color.WHITE, 10, 200,  SurfaceType.SAND));
-        list.add(new FootballBall(Color.GREEN, 10, 200,  Season.WINTER));
+        list.add(new VolleyballBall(Color.BLACK, 12, 200, SurfaceType.ASPHALT));
+        list.add(new FootballBall(Color.RED, 14, 200, Season.SUMMER));
+        list.add(new VolleyballBall(Color.WHITE, 10, 200, SurfaceType.SAND));
+        list.add(new FootballBall(Color.GREEN, 10, 200, Season.WINTER));
 
-//        list.sort(new BallTypeComparator());
-//        MergeSorter.mergeSort(list, new WeightComparator().thenComparing(new SizeComparator()));////////////////////
-//        QuickSorter.quickSort(list, new WeightComparator().thenComparing(new SizeComparator()));
-//        HeapSorter.heapSort(list, new WeightComparator().thenComparing(new BallTypeComparator()));
-        var sorter = new QuickSorter();
-        sorter.sort(list, new WeightComparator().thenComparing(new BallTypeComparator()));
+
+        var sorter = new HeapSorter();
+        sorter.sort(list, new ColorComparator());
         System.out.println(list);
     }
 }
